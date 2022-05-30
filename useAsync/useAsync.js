4f56de29a04fad3fa6e22d865ab4eef620a2afc0
Jayakrishnan <jayakrishnan1012@gmail.com>
2022-05-30 13:34:53 +0530

    Add async api hook for handling loading states

---
+++ useAsync/useAsync.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, useCallback } from "react";
+// Usage
+function App() {
+  const { execute, status, value, error } = useAsync(myFunction, false);
+  return (
+    <div>
+      {status === "idle" && <div>Start your journey by clicking a button</div>}
+      {status === "success" && <div>{value}</div>}
+      {status === "error" && <div>{error}</div>}
+      <button onClick={execute} disabled={status === "pending"}>
+        {status !== "pending" ? "Click me" : "Loading..."}
+      </button>
+    </div>
+  );
+}
+const myFunction = () => {
+  return new Promise((resolve, reject) => {
+    // call API here
+    setTimeout(() => {
+      const rnd = Math.random() * 10;
+      rnd <= 5
+        ? resolve("Submitted successfully")
+        : reject("Oh no there was an error");
+    }, 2000);
+  });
+};
+// Hook
+const useAsync = (asyncFunction, immediate = true) => {
+  const [status, setStatus] = useState("idle");
+  const [value, setValue] = useState(null);
+  const [error, setError] = useState(null);
+
+  const execute = useCallback(() => {
+    setStatus("pending");
+    setValue(null);
+    setError(null);
+    return asyncFunction()
+      .then((response) => {
+        setValue(response);
+        setStatus("success");
+      })
+      .catch((error) => {
+        setError(error);
+        setStatus("error");
+      });
+  }, [asyncFunction]);
+  useEffect(() => {
+    if (immediate) {
+      execute();
+    }
+  }, [execute, immediate]);
+  return { execute, status, value, error };
+};
